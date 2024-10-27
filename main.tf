f7f50a326d0906851baf6f800d5a2a4f0ef34b9b
MadhavRKumar <madhavrkumar@gmail.com>
2024-10-27 17:46:38 -0400

    change instance class

--- main.tf
+++ main.tf
@@ -91,7 +91,7 @@ resource "aws_db_instance" "postgres" {
   storage_type                = "gp2"
   engine                      = "postgres"
   engine_version              = "16.4"
-  instance_class              = "db.t2.micro"
+  instance_class              = "db.m5"
   parameter_group_name        = "default.postgres16.4"
   manage_master_user_password = true
   username                    = "main"
